3cdd8e23974090b1761e7807c83c8e7a95640ebe
Joao Otavio <joao0otavio3@gmail.com>
2023-05-19 01:51:52 -0300

    função de media entre buscas

--- main.py
+++ main.py
@@ -4,11 +4,55 @@ from busca import bfs, dfs, dijkstra, a_estrela, best_first;
 from random import randint, seed;
 
 def main() -> None:
-    comparacao(2000, 3);
+    media(2000, 3);
+
+def media(n, k):
+    grafo = grafo_knn(n, k);
+
+    medias = {
+        'Busca em profundidade': [0, 0, 0],
+        'Busca em largura': [0, 0, 0],
+        'Algoritmo Best-First': [0, 0, 0],
+        'Algoritmo A*': [0, 0, 0],
+        'Algoritmo de Dijkstra': [0, 0, 0]
+    };
+
+    n_pontos = 10;
+    seed(1225);
+    # Acumula valores dos parâmetros para cada busca, para cada par de pontos.
+    for _ in range(0, n_pontos):
+        origem, destino = gera_origem_destino(grafo, n);
+        resultado = executa_buscas(grafo, origem, destino);
+        for key in resultado.keys():
+            # Quantidade de nós no caminho;
+            medias[key][0] += len(resultado[key][0]);
+            # Distância.
+            medias[key][1] += resultado[key][1];
+            # Tempo.
+            medias[key][2] += resultado[key][2];
+
+    # Divide pelo número de pontos.
+    for algoritmo in medias.values():
+        for i in range(0, len(algoritmo)):
+            algoritmo[i] /= n_pontos;
+
+    for nome, resultado in medias.items():
+        imprimir_media(nome, resultado);
+
+def imprimir_media(algoritmo, resultado):
+    print('=' * 100);
+    print(algoritmo.center(100))
+    print('=' * 100);
+
+    print(f'Média total vértices: {resultado[0]}');
+
+    print(f'Média distância: {resultado[1]:.2f}');
+
+    print(f'Média tempo: {resultado[2]:.6f} segundos');
 
 def comparacao(n, k):
     grafo = grafo_knn(n, k);
-    
+    seed(1225);
     origem, destino = gera_origem_destino(grafo, n);
 
     resultados = executa_buscas(grafo, origem, destino);
@@ -17,7 +61,6 @@ def comparacao(n, k):
         imprimir_resultado(grafo, algoritmo, resultado);
 
 def gera_origem_destino(grafo, n):
-    seed(1225);
     indice_origem = randint(0, n);
     indice_destino = randint(0, n);
     while indice_origem == indice_destino:
@@ -41,14 +84,15 @@ def imprimir_resultado(grafo, algoritmo, resultado):
     print(algoritmo.center(100))
     print('=' * 100);
 
-    print("Caminho: ", end='');
+    print('Caminho: ', end='');
     for vertice in resultado[0]:
         print(grafo.vertices.index(vertice), end=' '); 
     print();
+    print(f'Total vértices: {len(resultado[0])}');
 
     print(f'Distância: {resultado[1]:.2f}');
 
-    print(f'Tempo: {resultado[2]:.5f} segundos');
+    print(f'Tempo: {resultado[2]:.6f} segundos');
 
 if __name__ == '__main__':
     main();
